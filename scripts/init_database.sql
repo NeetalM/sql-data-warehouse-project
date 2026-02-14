53ac6596cddc9a72908f845223124eff97780e0a
Neetal M <neetal.mistry@gmail.com>
2026-02-13 18:25:05 -0800

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+/* 
+==========================================
+CREATE DATABASE AND SCHEMA
+==========================================
+*/
+
+USE Master;
+CREATE DATABSE DataWarehouse;
+USE DataWarehouse;
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
